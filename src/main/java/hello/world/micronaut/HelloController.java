fe61515e06d50157193ecde9d4e6a57771630703
Omar Bautista <obautista@skytouchtechnology.com>
2018-10-12 14:09:35 -0700

    add HelloWorld example with Micronaut

---
+++ src/main/java/hello/world/micronaut/HelloController.java
@@ -0,0 +1,19 @@
+package hello.world.micronaut;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello")
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Get(value = "/", produces = "text/plain")
+    public String sayHello(){
+        logger.info("Calling sayHello action");
+        return "Hello World from micronaut.";
+    }
+}
