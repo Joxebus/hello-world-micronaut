20b1e9f75fff0f8c9c5470ea20950a7af52fe14f
Omar Bautista <obautista@skytouchtechnology.com>
2018-11-08 00:23:24 -0700

    Change HelloController path to /

--- src/main/java/hello/world/micronaut/HelloController.java
+++ src/main/java/hello/world/micronaut/HelloController.java
@@ -6,12 +6,12 @@ import org.slf4j.LoggerFactory;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
-@Controller("/hello")
+@Controller("/")
 public class HelloController {
 
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    @Get(value = "/", produces = "text/plain")
+    @Get(produces = "text/plain")
     public String sayHello(){
         logger.info("Calling sayHello action");
         return "Hello World from micronaut.";
